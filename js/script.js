e5dd41f1b20716b23cabdf17b6726d192eb7c730
abdullahbyrmv <abdullah.bayramov@inbox.ru>
2023-11-20 21:25:38 +0400

    Update

--- js/script.js
+++ js/script.js
@@ -6,4 +6,19 @@ async function displayData(){
     const response = await fetch(url);
     let data = await response.json();
     console.log(data);
+
+    let brandName = document.querySelector(".brand");
+    let category = document.querySelector(".category");
+    let description = document.querySelector(".description");
+    let price = document.querySelector(".price");
+    let discountPercentage = document.querySelector(".discountPercentage");
+    let image = document.querySelector(".image");
+
+
+    brandName.innerHTML = "<b>Brand: </b> " + data.products[0].brand;
+    category.innerHTML = "<b>Category: </b>" + data.products[0].category;
+    description.innerHTML = "<b>Description: </b>" + data.products[0].description;
+    price.innerHTML = "<b>Price: </b>" + data.products[0].price  + " $";
+    discountPercentage.innerHTML = "<b>Discount Percentage: </b>" + data.products[0].discountPercentage + " %";
+    image.setAttribute("src",`${data.products[0].images[0]}`);
 }
