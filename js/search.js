cac2f08cabc0e77d98ff9a699d20cf3208874780
abdullahbyrmv <abdullah.bayramov@inbox.ru>
2023-11-24 22:22:29 +0400

    Update

---
+++ js/search.js
@@ -0,0 +1,19 @@
+function search(){
+    const searchBoxValue = document.getElementById("search_bar").value.toLowerCase();
+    const products = document.getElementById("container");
+    const allProducts = document.querySelectorAll(".products");
+    const productTitle = products.getElementsByTagName("h2");
+
+    for(let i = 0 ; i < productTitle.length ; i++){
+        let cmp = allProducts[i].getElementsByTagName('h2')[0];
+        if(cmp != null || cmp !=undefined){
+            let textValue = cmp.textContent || cmp.innerHTML;
+            if(textValue.toLowerCase().includes(searchBoxValue)){
+                allProducts[i].style.display = "";
+            }
+            else{
+                allProducts[i].style.display="none";
+            }
+        }
+    }
+}
